e79787f6963edfa8691f3c4c4b1753dc2b1eb69e
EzArsn <95389651+EzArsn@users.noreply.github.com>
2022-01-12 13:42:08 +0530

    Add files via upload

---
+++ Chp 04/Ex 01.py
@@ -0,0 +1,12 @@
+# Exercise 1: Run the program on your system and see what numbers
+# you get. Run the program more than once and see what numbers you
+# get.
+
+import random
+def main():
+    for i in range(10):
+        x=random.random()
+        print(x)
+main()
+
+#After running more than once, it is found that no number is repeating itself whenever they're run.
---
+++ Chp 04/Ex 03.py
@@ -0,0 +1,27 @@
+# repeat_lyrics()
+
+# def print_lyrics():
+#     print("I'm a lumberjack, and I'm okay.")
+#     print('I sleep all night and I work all day.')
+
+
+# def repeat_lyrics():
+#     print_lyrics()
+#     print_lyrics()
+
+# Exercise 3: Move the function call back to the bottom and move the
+# definition of print_lyrics after the definition of repeat_lyrics. What
+# happens when you run this program?
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print('I sleep all night and I work all day.')
+
+repeat_lyrics()
+
+# Soln: Def can be done at any order, but before calling definition has to be done.
+# Code works perfectly
